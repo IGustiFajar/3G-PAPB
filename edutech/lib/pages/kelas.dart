e6db5e5dc2d92a4fe38f8c5b44f0f7ccdd4a024c
IGustiFajar <73781057+IGustiFajar@users.noreply.github.com>
2022-12-07 03:25:09 +0800

    Add Search dan Filter kelas

---
+++ edutech/lib/pages/kelas.dart
@@ -0,0 +1,24 @@
+class kelas {
+  final String judul;
+  final String UrlImage;
+
+  const kelas({
+    required this.judul,
+    required this.UrlImage,
+  });
+}
+
+const allkelas = [
+  kelas(
+      judul: 'Kelas UI/UX Designer : Level Beginner',
+      UrlImage: 'images/imagePop.png'),
+  kelas(
+      judul: 'Kelas UI/UX Designer : Level Intermediate',
+      UrlImage: 'images/imagePop2.png'),
+  kelas(
+      judul: 'Kelas Fullstack Developer : Level Beginner',
+      UrlImage: 'images/imagePop3.png'),
+  kelas(
+      judul: 'Kelas Product Management : Level Beginner',
+      UrlImage: 'images/imagePop.png')
+];
--- edutech/lib/pages/searchPage.dart
+++ edutech/lib/pages/searchPage.dart
@@ -1,3 +1,4 @@
+import 'package:edutech/pages/kelas.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -10,6 +11,9 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final controller = TextEditingController();
+  List<kelas> Kelas = allkelas;
+
   @override
   Widget build(BuildContext context) {
     final mediaQueryHeight = MediaQuery.of(context).size.height;
@@ -33,6 +37,56 @@ class _SearchPageState extends State<SearchPage> {
           )
         ],
       ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Nama Kelas',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(color: Colors.white10),
+                ),
+              ),
+              onChanged: searchKelas,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              itemCount: Kelas.length,
+              itemBuilder: ((context, index) {
+                final kelas = Kelas[index];
+                return ListTile(
+                  leading: Image.asset(
+                    kelas.UrlImage,
+                    fit: BoxFit.cover,
+                    width: 50,
+                    height: 50,
+                  ),
+                  title: Text(kelas.judul),
+                  onTap: () {
+                    Get.toNamed('/kelas-detail');
+                  },
+                );
+              }),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void searchKelas(String query) {
+    final suggestions = allkelas.where((kelas) {
+      final kelasJudul = kelas.judul.toLowerCase();
+      final input = query.toLowerCase();
+
+      return kelasJudul.contains(input);
+    }).toList();
+    setState(() => Kelas = suggestions);
   }
 }
