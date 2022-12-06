8968ffa48cb252116c6ed6c323efde664ba90db1
IGustiFajar <73781057+IGustiFajar@users.noreply.github.com>
2022-12-06 23:37:30 +0800

    Get Sertifikat dari Firebase Storage

--- edutech/lib/pages/sertifikatPage.dart
+++ edutech/lib/pages/sertifikatPage.dart
@@ -1,13 +1,32 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:edutech/pages/kelasPage.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
 
-class sertifikatPage extends StatelessWidget {
+class sertifikatPage extends StatefulWidget {
   const sertifikatPage({super.key});
 
   @override
+  State<sertifikatPage> createState() => _sertifikatPageState();
+}
+
+class _sertifikatPageState extends State<sertifikatPage> {
+  late Future<ListResult> futureFiles;
+
+  @override
+  void initState() {
+    super.initState();
+
+    futureFiles = FirebaseStorage.instance.ref('/sertifikat').listAll();
+  }
+
   Widget build(BuildContext context) {
     final mediaQueryHeight = MediaQuery.of(context).size.height;
 
@@ -77,12 +96,16 @@ class sertifikatPage extends StatelessWidget {
                           TextStyle(fontFamily: 'InterSemiBold', fontSize: 16),
                     )),
                 Spacer(),
-                Container(
-                    width: 36,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey, width: 1),
-                        borderRadius: BorderRadius.all(Radius.circular(6))),
-                    child: Image.asset('images/download.png')),
+                // Container(
+                //   width: 36,
+                //   decoration: BoxDecoration(
+                //     border: Border.all(color: Colors.grey, width: 1),
+                //     borderRadius: BorderRadius.all(
+                //       Radius.circular(6),
+                //     ),
+                //   ),
+                // ),
+                // Image.asset('images/download.png')),
                 Spacer(),
                 Container(
                     width: 36,
@@ -98,10 +121,66 @@ class sertifikatPage extends StatelessWidget {
               style: TextStyle(fontFamily: 'InterSemiBold', fontSize: 20),
             ),
             SizedBox(height: bodyHeight * 0.03),
-            Image.asset('images/sertifikat.png')
+            Image.asset('images/sertifikat.png'),
+            FutureBuilder<ListResult>(
+              future: futureFiles,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final files = snapshot.data!.items;
+                  return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: files.length,
+                    itemBuilder: (context, index) {
+                      final file = files[index];
+                      return ListTile(
+                        title: Text(file.name),
+                        trailing: GestureDetector(
+                          child: Image.asset('images/download.png'),
+                          onTap: () => downloadFile(file),
+                        ),
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Error nih'),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Future downloadFile(Reference ref) async {
+    // final dir = await getApplicationDocumentsDirectory();
+    // final file = File('${dir.path}/${ref.name}');
+
+    final url = await ref.getDownloadURL();
+
+    final tempdir = await getTemporaryDirectory();
+    final path = '${tempdir.path}/${ref.name}';
+    await Dio().download(url, path);
+
+    if (url.contains('.mp4')) {
+      await GallerySaver.saveVideo(path, toDcim: true);
+    } else if (url.contains('.png')) {
+      await GallerySaver.saveImage(path, toDcim: true);
+    } else if (url.contains('.jpg')) {
+      await GallerySaver.saveImage(path, toDcim: true);
+
+      // await ref.writeToFile(file);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Downloaded ${ref.name}'),
+        ),
+      );
+    }
   }
 }
